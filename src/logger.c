4f1d6676619159f2df131f9d63e7d39f58b2c8e5
GabeQV <gabeqv16@gmail.com>
2026-04-29 14:23:39 -0600

    Updating logger.c

--- src/logger.c
+++ src/logger.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-#include <time.h>
 #include "logger.h"
 #include "sincronizacion.h"
+#include <stdio.h>
+#include <time.h>
 
-static FILE *archivo_log;
+static FILE *archivo_log = NULL;
 
 void logger_init(void) {
     archivo_log = fopen("terminal.log", "w");
-    if (!archivo_log)
+    if (!archivo_log) {
+        perror("fopen log");
         archivo_log = stdout;
-    fprintf(archivo_log, "%-7s %-12s %-14s %s\n", "ID", "ESTADO", "TIEMPO", "MENSAJE");
+    }
+    fprintf(archivo_log, "%-6s %-12s %-14s %s\n",
+            "ID", "ESTADO", "TIEMPO", "MENSAJE");
+    fprintf(archivo_log, "----------------------------------------------\n");
 }
 
 void logger_destroy(void) {
@@ -20,11 +24,15 @@ void logger_destroy(void) {
 void log_evento(int camion_id, const char *estado, const char *mensaje) {
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
-    double tiempo = ts.tv_sec + ts.tv_nsec / 1e9;
+    double t = ts.tv_sec + ts.tv_nsec / 1e9;
 
     pthread_mutex_lock(&mutex_log);
-    fprintf(archivo_log, "C%-5d %-12s %-14.4f %s\n", camion_id, estado, tiempo, mensaje);
+
+    fprintf(archivo_log, "C%-5d %-12s %-14.4f %s\n",
+            camion_id, estado, t, mensaje);
     fflush(archivo_log);
-    printf("C%-5d %-12s %-14.4f %s\n", camion_id, estado, tiempo, mensaje);
+
+    printf("C%-5d %-12s %-14.4f %s\n", camion_id, estado, t, mensaje);
+
     pthread_mutex_unlock(&mutex_log);
 }
